d9804dec7f62163cab1a64a5f5237af90b1e8d9e
Dhanush Dinesh <dhanushndinesh@gmail.com>
2024-08-18 03:07:50 -0500

    Added private subnet A and B

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -84,3 +84,32 @@ resource "aws_route" "public_internet_public_b" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+
+###########################################
+# Private Subnet for internal access only #
+###########################################
+
+# Subnet A
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+# Subnet B
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
