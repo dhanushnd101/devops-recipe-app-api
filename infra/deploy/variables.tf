7d2124b341ae0c4835ebdcfefa2be2dc8c37b431
Dhanush Dinesh <dhanushndinesh@gmail.com>
2024-09-15 20:10:50 -0500

    added RDS instance in TF

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "contact" {
   description = "The contact information used by the default tags"
   default     = "dhanushndinesh@gmail.com"
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
